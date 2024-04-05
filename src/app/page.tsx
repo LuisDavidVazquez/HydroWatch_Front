06c8166da1506d008ec1e80a11e3bbbbdf642af1
Luis David <luisdvazquez17@gmail.com>
2024-04-04 20:00:27 -0600

    Prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import Axios from 'axios'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [user, setUser] = useState({})
+  
+  const registro = async (e : any) => {
+
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget);
+    try {
+      const res = await Axios.post("http://localhost:4000/users", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      data: {
+        name: formData.get("name"),
+        lastname : formData.get("lastname"),
+        role : formData.get("role"),
+      }
+    });
+    if(res.data){
+      console.log(res.data);
+      setUser(res.data)
+      alert("Se registro el usuario correctamente")
+    }else {
+      alert("No se pudo registrar el usuario, revise los campos")      
+    }
+    } catch (error) {
+      alert("Error al procesar la peticion")
+    }
+    
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main >
+      <form action="" onSubmit={registro}>
+        <h1>Registro(Crear ususario)</h1><br />
+        <input type="text" placeholder='nombre' name="name" /><br />
+        <input type="text" placeholder='apellido' name="lastname" /><br />
+        <input type="text" placeholder='cargo' name="role" /><br />
+        <input type="text" placeholder='correo' name="email" /><br />
+        <input type="password" placeholder='contraseña' name="password" /><br />
+        <button>Registrar</button>
+      </form>
     </main>
   );
 }
